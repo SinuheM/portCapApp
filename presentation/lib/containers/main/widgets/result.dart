562f230ded8cdfd0a2f56f13e828aca2cc774a2c
Adrian Priale <sinuhe-10@hotmail.com>
2024-06-02 00:38:30 -0500

    Image

--- presentation/lib/containers/main/widgets/result.dart
+++ presentation/lib/containers/main/widgets/result.dart
@@ -48,6 +48,19 @@ class _ResultCardState extends State<ResultCard> {
     }
   }
 
+  getImage() {
+    final value = double.parse(form.control('loadAdmissible').value ?? 0.0);
+    if (value < 0.8) {
+      return 'assets/images/p_cimentacion.png';
+    } else if (value >= 0.8 && value < 1.2) {
+      return 'assets/images/z_combinada.jpg';
+    } else if (value >= 1.2 && value < 1.5) {
+      return 'assets/images/z_conectada.jpg';
+    } else {
+      return 'assets/images/z_aislada.png';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ReactiveForm(
@@ -58,10 +71,9 @@ class _ResultCardState extends State<ResultCard> {
           RowInfo(title: getResult()),
           Container(
             height: 160,
-            decoration: const BoxDecoration(
+            decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage(
-                    'assets/images/dibujo_app-removebg-preview.png'), // ruta de la imagen
+                image: AssetImage(getImage()), // ruta de la imagen 
                 fit: BoxFit.contain, // ajustar la imagen al contenedor
               ),
             ),
